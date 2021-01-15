066113c7485c32ef6c75da4b3850de4d635ec7fb
Nishant-Pall <palln6935@gmail.com>
2021-01-15 13:38:35 +0530

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import './App.css';
-import Greet from './components/Greet'
+// import Greet from './components/Greet'
 // import Welcome from './components/Welcome'
 // import Hello from './components/Hello'
 // import Message from './components/Message'
 // import Counter from './components/Counter'
-import ClassClick from './components/ClassClick';
-import FunctionClick from './components/FunctionClick'
-import EventBind from './components/EventBind'
-import ParentComponent from './components/ParentComponent';
+// import ClassClick from './components/ClassClick';
+// import FunctionClick from './components/FunctionClick'
+// import EventBind from './components/EventBind'
+// import ParentComponent from './components/ParentComponent';
+import UserGreetings from './components/UserGreetings';
 
 function App() {
   return (
     <div className="App">
-      <ParentComponent />
+      <UserGreetings />
+      {/* <ParentComponent /> */}
       {/* <FunctionClick /> */}
       {/* <ClassClick /> */}
       {/* <EventBind /> */}
       {/* <Counter /> */}
-      <Greet name="Bruce" heroName="Batman" >
+      {/* <Greet name="Bruce" heroName="Batman" >
         <p>This is cHiLdReN props</p>
-      </Greet>
+      </Greet> */}
       {/* <Greet name="Clark" heroName="Superman" >
         <button>Action</button>
       </Greet> */}
---
+++ src/components/UserGreetings.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+
+class UserGreetings extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            isLoggedIn: true
+        }
+    }
+    render() {
+        return this.state.isLoggedIn && <div>Welcome Nishant</div>
+    }
+    // return this.state.isLoggedIn ?
+    //     <div>Welcome Nishant</div> :
+    //     <div>Welcome Guest</div>
+    // let message
+    // if (this.state.isLoggedIn) {
+    //     message = <div>Welcome Nishant</div>
+    // }
+    // else
+    //     message = <div>Welcome Guest</div>
+
+    // return <div>{message}</div>
+    // if (this.state.isLoggedIn) {
+    //     return (
+    //         <div>
+    //             Welcome Nishant
+    //         </div>
+    //     )
+    // }
+    // else
+    //     return (
+    //         <div>
+    //             Welcome Guest
+    //         </div>
+    // return (
+    //     <div>
+    //         <div>Welcome Nishant</div>
+    //         <div>Welcome Guest</div>
+    //     </div>
+    // )
+}
+
+export default UserGreetings
