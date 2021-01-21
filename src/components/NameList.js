2b920662317d4af1a16be4c82d60bd92414c9c9e
Nishant-Pall <palln6935@gmail.com>
2021-01-21 12:07:54 +0530

    Index as key

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Person from './Person'
 
 function NameList() {
+    const names = ['Bruce', 'Clark', 'Diana']
     const persons = [
         {
             id: 1,
@@ -22,12 +23,24 @@ function NameList() {
             skill: 'Vue'
         }
     ]
+    // Keys give elements a stable identity, we should include
+    // keys when creating lists of elements
+    // Keys help React identify which items have changed, are added, or removed,
+    // This improves efficiency
     const personList = persons.map(person =>
         <Person key={person.id} person={person} />
     )
+    // map has two arguments, name and index.
+    // index can be used as key if we don't have any unique properties in names array
+    // although keys can cause UI issues and react might no render data correctly
+    // We should use index as key when:
+    // Items in list do not have a unique id
+    // List is static and wont change
+    // List is never reordered or filtered
+    const nameList = names.map((name, index) => <h2 key={index}>{index} {name}</h2>)
     return (
         <div>
-            {personList}
+            {nameList}
         </div>
     )
 }
