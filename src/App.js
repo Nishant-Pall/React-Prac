ec5e09e322cc89aca9b963e9fda223104e072f7d
Nishant-Pall <palln6935@gmail.com>
2021-01-23 15:29:48 +0530

    add Form Handling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Inline from './components/Inline';
 import './appStyles.css'
 import styles from './appStyles.module.css'
+import Form from './components/Form';
 // import NameList from './components/NameList';
 // import Stylesheet from './components/Stylesheet';
 // import Greet from './components/Greet'
@@ -18,8 +19,9 @@ import styles from './appStyles.module.css'
 function App() {
   return (
     <div className="App">
-      <h1 className='error'>Error</h1>
-      <h1 className={styles.success}>Success</h1>
+      {<Form />}
+      {/* <h1 className='error'>Error</h1> */}
+      {/* <h1 className={styles.success}>Success</h1> */}
       {/* {<Inline />} */}
       {/* {<Stylesheet primary={1} />} */}
       {/* {<NameList />} */}
