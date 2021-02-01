c5d994cac8f2b86bd6397e9901714f3cc3342dfa
Nishant-Pall <palln6935@gmail.com>
2021-02-01 12:43:12 +0530

    Add component update lifecycle methods and document the methods

--- src/components/LifecycleA.js
+++ src/components/LifecycleA.js
@@ -12,6 +12,10 @@ class LifecycleA extends Component {
         console.log('LifeCycleA constructor');
     }
 
+    // Method is called every time a component is re-rendered
+    // Either returns null or an object that represents updated
+    // state of the component
+    // Used when state depends on the props of the component
     static getDerivedStateFromProps(props, state) {
         console.log('LifecycleA getDerivedStateFromProps');
         return null
@@ -21,11 +25,45 @@ class LifecycleA extends Component {
         console.log('LifecycleA componentDidMount');
     }
 
+    // Receives updated props and state
+    // Dictates should the component re-render or not
+    // It can prevent method that causes re-rendering of component
+    // everytime props or the state changes for performance optimization
+    // We can compare existing and next props and state values
+    // and return true or false letting react know whether to
+    // update the component or not
+    shouldComponentUpdate() {
+        console.log('LifecycleA shouldComponentUpdate')
+        return true
+    }
+
+    // Called right before the changes from virtual DOM are to 
+    // be reflected in the DOM
+    // Used to capture info from DOM, example scroll position
+    // returns a value or null
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log('LifecycleA getSnapshotBeforeUpdate')
+        return null
+    }
+
+    // Called after the render is finished in the re-render cycles
+    // We can make ajax calls in it
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('LifecycleA componentDidUpdate')
+    }
+
+    changeState = () => {
+        this.setState({
+            name: 'Codevolution'
+        })
+    }
+
     render() {
         console.log('LifecyleA render')
         return (
             <div>
                 <div>LifeCycleA</div>
+                <button onClick={this.changeState}>Change state</button>
                 <LifecycleB />
             </div>
         )
